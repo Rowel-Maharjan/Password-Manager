6e270f7a7ceafc0c3cae93564f3835ab189d18fe
Rowel Maharjan <rowelmhj@gmail.com>
2024-07-18 16:18:45 +0545

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='underline text-5xl text-red-500 cursor-pointer hover:text-6xl'>
+        MY UI
+      </div>
+    </>
+  )
+}
+
+export default App
