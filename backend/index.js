631ed2799d661db21f1f9cab236e648edb7edebd
Rowel Maharjan <rowelmhj@gmail.com>
2024-07-27 11:50:21 +0545

    Minor change

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express, { urlencoded } from 'express';
 import mongoose from 'mongoose';
-import passwordController  from './controllers/password.controller.js'
+import passwordController from './controllers/password.controller.js'
 import bodyParser from 'body-parser'
-const {createPassword, deletePassword, editPassword, getPassword} = passwordController;
+const { createPassword, deletePassword, editPassword, getPassword } = passwordController;
 import 'dotenv/config'
 import cors from 'cors'
 
@@ -12,7 +12,6 @@ app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongodb://localhost:27017/
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("connection success")
