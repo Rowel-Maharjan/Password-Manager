409f7ad21793e16700f8b7103984aed67be8e491
Rowel Maharjan <rowelmhj@gmail.com>
2024-10-25 17:41:06 +0545

    From render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import './App.css'
 import Navbar from './components/Navbar'
-import Manager from './components/Manager'
 import React_form_hook from './components/React_form_hook'
 import Footer from './components/Footer'
 
