ad8fc14f17430e35f372504f99178ca84611e4c2
Rowel Maharjan <rowelmhj@gmail.com>
2024-10-25 17:02:18 +0545

    Changes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import passwordController from './controllers/password.controller.js'
 import bodyParser from 'body-parser'
-const { createPassword, deletePassword, editPassword, getPassword } = passwordController;
 import 'dotenv/config'
 import cors from 'cors'
+
+const { createPassword, deletePassword, editPassword, getPassword } = passwordController;
 
 const app = express();
 const port = 3000
@@ -12,16 +13,18 @@ const port = 3000
 app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
-mongoose.connect(process.env.MONGO_URL)
+
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("connection success")
-        app.listen(port, () =>
-            console.log('Example app listening on port 3000!'),
-        );
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
     })
     .catch(() => {
-        console.log("Error")
+        console.log("Error in connection of Database")
     })
 
 
